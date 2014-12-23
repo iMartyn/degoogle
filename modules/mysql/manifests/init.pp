2568c83ff84bb1a8ad78f14d21e9a9ee62d2abcc
Martyn Ranyard <ranyardm@gmail.com>
2014-12-23 19:15:54 +0000

    Moving to Hiera file to make provisioning without breaking the repo easier.

--- modules/mysql/manifests/init.pp
+++ modules/mysql/manifests/init.pp
@@ -1,10 +1,18 @@
 class mysql {
 
   # root mysql password
-  $mysqlpw = "d3v0p5"
+  $mysqlpw = hiera('mysql_root_password')
+  $mailpw = hiera('mysql_mail_password')
+#"d3v0p5"
 
   # install mysql server
   package { "mysql-server":
+    ensure => present,
+    require => Exec["apt-get update"]
+  }
+
+  # install mysql-client 
+  package { "mysql-client":
     ensure => present,
     require => Exec["apt-get update"]
   }
@@ -19,6 +27,12 @@ class mysql {
   exec { "set-mysql-password":
     unless => "mysqladmin -uroot -p$mysqlpw status",
     command => "mysqladmin -uroot password $mysqlpw",
+    require => Service["mysql"],
+  }
+
+  exec { "set-mysql-mail-password":
+    unless => "mysqladmin -umail -p$mailpw status",
+    command => "mysql -uroot -p$mysqlpw -c 'CREATE USER `mail`@`localhost` IDENTIFIED BY \"$mailpw\"; GRANT ALL PRIVILEGES ON `mail`.* TO `mail`@`localhost`'",
     require => Service["mysql"],
   }
 }
