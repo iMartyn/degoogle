6ebb99fbffe964059d38402bb8dbd3af67ea282f
Martyn Ranyard <ranyardm@gmail.com>
2014-12-26 13:42:57 +0000

    Configuration as on vps

--- modules/dovecot/manifests/config.pp
+++ modules/dovecot/manifests/config.pp
@@ -1,5 +1,6 @@
 class dovecot::config {
     $mailhostname = hiera('mailhostname')
+    $domain = hiera('domain')
     file{ 'dovecot.conf':
         path => "/etc/dovecot/dovecot.conf",
         content => template("dovecot/dovecot.conf.erb"),
@@ -21,6 +22,48 @@ class dovecot::config {
     file{ 'dovecot-10-ssl.conf':
         path => "/etc/dovecot/conf.d/10-ssl.conf",
         content => template("dovecot/10-ssl.conf.erb"),
+        require => Package['dovecot-imapd']
+    }
+
+    file{ 'dovecot-10-mail.conf':
+        path => "/etc/dovecot/conf.d/10-mail.conf",
+        content => template("dovecot/10-mail.conf.erb"),
+        require => Package['dovecot-imapd']
+    }
+
+    file{ 'dovecot-10-auth.conf':
+        path => "/etc/dovecot/conf.d/10-auth.conf",
+        content => template("dovecot/10-auth.conf.erb"),
+        require => Package['dovecot-imapd']
+    }
+
+    file{ 'dovecot-15-lda.conf':
+        path => "/etc/dovecot/conf.d/15-lda.conf",
+        content => template("dovecot/15-lda.conf.erb"),
+        require => Package['dovecot-imapd']
+    }
+
+    file{ 'dovecot-20-imap.conf':
+        path => "/etc/dovecot/conf.d/20-imap.conf",
+        content => template("dovecot/20-imap.conf.erb"),
+        require => Package['dovecot-imapd']
+    }
+
+    file{ 'dovecot-20-managesieve.conf':
+        path => "/etc/dovecot/conf.d/20-managesieve.conf",
+        content => template("dovecot/20-managesieve.conf.erb"),
+        require => Package['dovecot-imapd']
+    }
+
+    file{ 'dovecot-90-plugin.conf':
+        path => "/etc/dovecot/conf.d/90-plugin.conf",
+        content => template("dovecot/90-plugin.conf.erb"),
+        require => Package['dovecot-imapd']
+    }
+
+    file{ 'dovecot-auth-master.conf.ext':
+        path => "/etc/dovecot/conf.d/auth-master.conf.ext",
+        content => template("dovecot/auth-master.conf.ext.erb"),
         require => Package['dovecot-imapd']
     }
 }
