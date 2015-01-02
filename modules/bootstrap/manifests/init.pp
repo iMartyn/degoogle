c3b32b9b2e64d5c4e5ec0be0ac239772cc27e939
Martyn Ranyard <ranyardm@gmail.com>
2015-01-02 20:41:29 +0000

    I AM the magic bash one-liner man

--- modules/bootstrap/manifests/init.pp
+++ modules/bootstrap/manifests/init.pp
@@ -6,7 +6,9 @@ class bootstrap {
   }
 
   # ensure local apt cache index is up to date before beginning
+  # the magic unless line is to only do this once per day!
   exec { 'apt-get update':
-    command => '/usr/bin/apt-get update'
+    command => '/usr/bin/apt-get update',
+    unless => 'test $(expr `date +%s` - `stat -c %Y /var/cache/apt/`) -lt 36000'
   }
 }
