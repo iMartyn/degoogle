c7631696083cbcdda96f55fe8152078683bf8a3b
Martyn Ranyard <ranyardm@gmail.com>
2015-01-02 13:29:21 +0000

    Adds more dependancies explicitly

--- modules/ssl/manifests/init.pp
+++ modules/ssl/manifests/init.pp
@@ -1,4 +1,7 @@
 class ssl {
+    file { '/etc/ssl':
+        ensure => 'directory',
+    }
     file { '/etc/ssl/mycerts':
         ensure => 'directory',
         mode => 700,
@@ -12,36 +15,42 @@ class ssl {
         source => "puppet:///modules/ssl/$mailhostname.key",
         owner => 'root',
         group => 'root',
-        mode => 600
+        mode => 600,
+        notify => [ Service['nginx'], Service['dovecot'], Service['postfix'] ]
     }
     file { "/etc/ssl/mycerts/$mailhostname.pem":
         source => "puppet:///modules/ssl/$mailhostname.pem",
         owner => 'root',
         group => 'root',
-        mode => 600
+        mode => 600,
+        notify => [ Service['nginx'], Service['dovecot'], Service['postfix'] ]
     }
     file { "/etc/ssl/private/$cloudhostname.key":
         source => "puppet:///modules/ssl/$cloudhostname.key",
         owner => 'root',
         group => 'root',
-        mode => 600
+        mode => 600,
+        notify => Service['nginx']
     }
     file { "/etc/ssl/mycerts/$cloudhostname.pem":
         source => "puppet:///modules/ssl/$cloudhostname.pem",
         owner => 'root',
         group => 'root',
-        mode => 600
+        mode => 600,
+        notify => Service['nginx']
     }
     file { "/etc/ssl/private/$pfahostname.key":
         source => "puppet:///modules/ssl/$pfahostname.key",
         owner => 'root',
         group => 'root',
-        mode => 600
+        mode => 600,
+        notify => Service['nginx']
     }
     file { "/etc/ssl/mycerts/$pfahostname.pem":
         source => "puppet:///modules/ssl/$pfahostname.pem",
         owner => 'root',
         group => 'root',
-        mode => 600
+        mode => 600,
+        notify => Service['nginx']
     }
 }
