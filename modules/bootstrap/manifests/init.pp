c7c63148b8f13b5773f18ac56ae0c2e01df12c46
Martyn Ranyard <ranyardm@gmail.com>
2015-01-03 00:39:37 +0000

    Too magic.  Will need to find a new magic node.

--- modules/bootstrap/manifests/init.pp
+++ modules/bootstrap/manifests/init.pp
@@ -9,6 +9,5 @@ class bootstrap {
   # the magic unless line is to only do this once per day!
   exec { 'apt-get update':
     command => '/usr/bin/apt-get update',
-    unless => 'test $(expr `date +%s` - `stat -c %Y /var/cache/apt/`) -lt 36000'
   }
 }
