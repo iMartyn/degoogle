691b9df7e13dabd216d2685dbfcdbc661d1737e9
Martyn Ranyard <martyn@theendofhistether.org.uk>
2015-12-23 23:51:35 +0000

    Create the letsencrypt user

--- modules/ssl/manifests/init.pp
+++ modules/ssl/manifests/init.pp
@@ -4,53 +4,27 @@ class ssl {
     }
     file { '/etc/ssl/mycerts':
         ensure => 'directory',
+        mode => 770,
+        owner => 'root',
+        group => 'www-data'
+    }
+    file { '/etc/ssl/private':
+        ensure => 'directory',
         mode => 700,
         owner => 'root',
-        group => 'root'
+        group => 'www-data'
     }
     $mailhostname = hiera('mailhostname')
     $cloudhostname = hiera('cloudhostname')
     $pfahostname = hiera('pfahostname')
-    file { "/etc/ssl/private/$mailhostname.key":
-        source => "puppet:///modules/ssl/$mailhostname.key",
-        owner => 'root',
-        group => 'root',
-        mode => 600,
-        notify => [ Service['nginx'], Service['dovecot'], Service['postfix'] ]
+    user { 'letsencrypt':
+	ensure => 'present',
+	groups => 'www-data'
     }
-    file { "/etc/ssl/mycerts/$mailhostname.pem":
-        source => "puppet:///modules/ssl/$mailhostname.pem",
-        owner => 'root',
-        group => 'root',
-        mode => 600,
-        notify => [ Service['nginx'], Service['dovecot'], Service['postfix'] ]
-    }
-    file { "/etc/ssl/private/$cloudhostname.key":
-        source => "puppet:///modules/ssl/$cloudhostname.key",
-        owner => 'root',
-        group => 'root',
-        mode => 600,
-        notify => Service['nginx']
-    }
-    file { "/etc/ssl/mycerts/$cloudhostname.pem":
-        source => "puppet:///modules/ssl/$cloudhostname.pem",
-        owner => 'root',
-        group => 'root',
-        mode => 600,
-        notify => Service['nginx']
-    }
-    file { "/etc/ssl/private/$pfahostname.key":
-        source => "puppet:///modules/ssl/$pfahostname.key",
-        owner => 'root',
-        group => 'root',
-        mode => 600,
-        notify => Service['nginx']
-    }
-    file { "/etc/ssl/mycerts/$pfahostname.pem":
-        source => "puppet:///modules/ssl/$pfahostname.pem",
-        owner => 'root',
-        group => 'root',
-        mode => 600,
-        notify => Service['nginx']
+    file { '/home/letsencrypt':
+        ensure => 'directory',
+        mode => 700,
+        owner => 'letsencrypt',
+        group => 'www-data'
     }
 }
