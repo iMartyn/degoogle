e25b44237b98e956a9554960cb34028bdddcda93
Martyn Ranyard <martyn.ranyard@skybettingandgaming.com>
2016-09-22 18:21:46 +0100

    Fixup copy pasta

--- modules/redis/manifests/config.pp
+++ modules/redis/manifests/config.pp
@@ -1,9 +1,9 @@
-class dspam::config {
+class redis::config {
 
     file{ 'redis.conf':
         path => "/etc/redis/redis.conf",
         content => template("redis/redis.conf.erb"),
-        require => [ Service['mysql'], Exec[set-mysql-root-password], Package['dspam'] ],
+        require => [ Package['redis'] ],
     }
 
 }
