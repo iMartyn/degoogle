f52110584a2eca86c2c3fc8e03d66f68d47dd2b3
Martyn Ranyard <martyn.ranyard@skybettingandgaming.com>
2016-09-22 18:21:06 +0100

    Fixup copy pasta

--- modules/rmilter/manifests/config.pp
+++ modules/rmilter/manifests/config.pp
@@ -1,9 +1,3 @@
-class dspam::config {
-
-    file{ 'redis.conf':
-        path => "/etc/redis/redis.conf",
-        content => template("redis/redis.conf.erb"),
-        require => [ Service['mysql'], Exec[set-mysql-root-password], Package['dspam'] ],
-    }
+class rmilter::config {
 
 }
